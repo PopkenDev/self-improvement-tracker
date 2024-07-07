e0d8ca1b6d5e1bcc407ca82d5aaab80a7eb0498f
PopkenDev <popkendev@outlook.com>
2024-07-07 14:04:51 +0200

    Added dynamic modal ui

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,11 +1,24 @@
 "use client";
 
+import { Poppins } from "next/font/google";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
+
+const buttonVariants = {
+  default:
+    "bg-emerald-600 rounded-mdpy-2 px-4 font-normal transition text-sm text-white hover:opacity-80",
+  icon: "bg-emerald-600 w-10 h-10 rounded-full flex justify-center items-center transition hover:opacity-80",
+};
+
 interface ButtonProps {
   children: React.ReactNode;
   type: "submit" | "button" | "reset";
   disabled?: boolean;
   className?: string;
-  variant?: "primary" | "secondary" | "tertiary";
+  variant?: "default" | "icon";
 }
 
 export const Button = ({
@@ -13,12 +26,14 @@ export const Button = ({
   type,
   disabled = false,
   className,
+  variant,
 }: ButtonProps) => {
+  if (!variant) return null;
   return (
     <button
       type={type}
       disabled={disabled}
-      className={`${className} rounded-md py-2 font-semibold transition hover:opacity-80`}
+      className={`${className} ${font.className} ${buttonVariants[variant]} rounded-md py-2 font-semibold transition hover:opacity-80`}
     >
       {children}
     </button>
---
+++ components/ui/modal.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
+
+interface ModalProps {
+  children: React.ReactNode;
+  onClose: () => void;
+}
+
+export const Modal = ({ children, onClose }: ModalProps) => {
+  return (
+    <div className="light-shadow absolute left-1/2 top-1/2 h-[calc(100vh-32px)] w-[calc(100vw-32px)] -translate-x-1/2 -translate-y-1/2 rounded-md bg-[#242424]">
+      <FontAwesomeIcon
+        onClick={onClose}
+        icon={faClose}
+        className="absolute right-2 top-2 h-4 w-4 cursor-pointer text-white/70"
+      />
+      {children}
+    </div>
+  );
+};
